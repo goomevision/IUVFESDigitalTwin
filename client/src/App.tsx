32499619ea87e05272d6d3c408a42e6e28ad029d
Nazirach <37241835+Nazirach@users.noreply.github.com>
2026-08-10 19:13:36 +0700

    feat(hardware): require static hardware setup before control room

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,13 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
+import EntryGate from "./pages/EntryGate";
 import HardwareConfiguration from "./pages/HardwareConfiguration";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" component={EntryGate} />
       <Route path="/hardware" component={HardwareConfiguration} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
