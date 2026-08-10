6c6b92432455ca670ea67f179293bf126b513fcf
Nazirach <37241835+Nazirach@users.noreply.github.com>
2026-08-10 22:26:11 +0700

    feat(ui): make scientific intake the experiment entry gate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
+import ScientificExperimentFlow from "@/pages/ScientificExperimentFlow";
+import ExperimentReplay from "./pages/ExperimentReplay";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import ExperimentReplay from "./pages/ExperimentReplay";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" component={ScientificExperimentFlow} />
+      <Route path="/experiment" component={ScientificExperimentFlow} />
       <Route path="/replay/:experimentId" component={ExperimentReplay} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
